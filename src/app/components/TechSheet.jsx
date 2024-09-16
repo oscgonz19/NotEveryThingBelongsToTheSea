1883aa554a5a439d68aa833d2a01c3eebe79b13d
OzzDragon <oscargo1917@gmail.com>
2024-09-16 14:53:45 -0500

    techsheet update

--- src/app/components/TechSheet.jsx
+++ src/app/components/TechSheet.jsx
@@ -7,46 +7,29 @@ const TechSheet = () => {
         <h2 className="text-3xl font-bold mb-12 text-center text-gray-700">Ficha técnica</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-12">
           <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Actuación:</h3>
-            <p className="mt-2">
-              
-            </p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Escritura y Guion:</h3>
-            <p className="mt-2">Angelica Salas</p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Producido por:</h3>
+            <h3 className="text-lg font-semibold text-gray-400">Produccion e investigación:</h3>
             <p className="mt-2">Maria Camila Hernandez Toro</p>
           </div>
           <div className="col-span-1">
+            <h3 className="text-lg font-semibold text-gray-400">Guionista:</h3>
+            <p className="mt-2">Angelica Salas</p>
+          </div>
+          <div className="col-span-1">
             <h3 className="text-lg font-semibold text-gray-400">Coproducción:</h3>
-            <p className="mt-2"></p>
+            <p className="mt-2">Ylahiah Kaisal </p>
           </div>
              <div className="col-span-1">
             <h3 className="text-lg font-semibold text-gray-400">Director:</h3>
-            <p className="mt-2"> Juan Ruiz </p>
+            <p className="mt-2">  Juan Andrés Ruiz </p>
           </div>
           <div className="col-span-1">
             <h3 className="text-lg font-semibold text-gray-400">Dirección de fotografía:</h3>
             <p className="mt-2">Juan Camilo Franco</p>
+             <p className="mt-2">Cristian Mauricio López Gomez</p>
           </div>
           <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Dirección de arte:</h3>
-            <p className="mt-2"></p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Sonido:</h3>
-            <p className="mt-2"></p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Música original:</h3>
-            <p className="mt-2"></p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-semibold text-gray-400">Montaje:</h3>
-            <p className="mt-2"></p>
+            <h3 className="text-lg font-semibold text-gray-400">Sonidista:</h3>
+            <p className="mt-2">Andri Yulieth Meneses</p>
           </div>
         </div>
       </div>
